9bbf2d7a9d098ffd28d6a26d64f9205ec4155dad
jdsports\sashidhar.vakiti <sashidhar.vakiti@jdplc.com>
2019-05-22 15:47:15 +1000

    Added HTML Dashboard and Reporting controller

---
+++ JDAU_DashboardApp/JDAU_DashboardApp/Controllers/ReportingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JDAU_DashboardApp.Controllers
+{
+    public class ReportingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
